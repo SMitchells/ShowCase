da17425388a4d07f4d0c3499cc5041aa8924ab54
SMitchells <michelhelmer96@gmail.com>
2021-11-20 00:22:19 -0300

    Subindo o codigo com as ultimas alterações do curso.

--- NodeJS/noticias.js
+++ NodeJS/noticias.js
@@ -2,7 +2,20 @@ var http = require('http');
 
 var openServer = http.createServer((req, res) => {
 
-    
+    var categoria = req.url
+
+    categoria = categoria.replace('/', '')
+
+    if(categoria == 'tecnologia'){
+        res.end('<html><body>Noticias de Técnologia</body></html>');
+    }else if(categoria == 'moda'){
+        res.end('<html><body>Noticias de Moda</body></html>');
+    }else if(categoria == 'beleza'){
+        res.end('<html><body>Noticias de beleza</body></html>');
+    }else{
+        res.end('<html><body>Portal de Noticias</body></html>');
+    }
 });
 
+console.log('On-line...')
 openServer.listen(3000)
